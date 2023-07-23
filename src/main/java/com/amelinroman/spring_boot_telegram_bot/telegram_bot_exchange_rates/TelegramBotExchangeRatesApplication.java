80f8597a72f113f3233fb8ff9b53461f128d389e
AmelinRoman <94740458+AmelinRoman@users.noreply.github.com>
2023-07-23 17:26:21 +0300

    Initial commit

---
+++ src/main/java/com/amelinroman/spring_boot_telegram_bot/telegram_bot_exchange_rates/TelegramBotExchangeRatesApplication.java
@@ -0,0 +1,13 @@
+package com.amelinroman.spring_boot_telegram_bot.telegram_bot_exchange_rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotExchangeRatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotExchangeRatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amelinroman/spring_boot_telegram_bot/telegram_bot_exchange_rates/TelegramBotExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amelinroman.spring_boot_telegram_bot.telegram_bot_exchange_rates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotExchangeRatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
